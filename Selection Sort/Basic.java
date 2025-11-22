3ad87978ea4f53804d0520880402477bd485675a
Towhidul Islam Tarek <tarektowhidulislam@gmail.com>
2025-11-22 14:49:10 +0600

    Add files via upload

---
+++ Selection Sort/Basic.java
@@ -0,0 +1,33 @@
+package Selection_Sort;
+
+public class Basic {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " "); 
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[]= {7,8, 3,1,2};
+
+        for(int i = 0; i<arr.length-1; i++)
+        {
+            int smallerst = i;
+            for(int j = i+1; j<arr.length; j++)
+            {
+                if(arr[smallerst]> arr[j])
+                {
+                    smallerst = j;
+                }
+            }
+
+            int temp = arr[smallerst];
+            arr[smallerst] = arr[i];
+            arr[i]= temp;
+        }
+
+        printArray(arr);
+    }
+    
+}
